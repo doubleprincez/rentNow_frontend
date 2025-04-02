02aa87384b618029b6cf5db506581cd289d83537
Doubleprincez <adenekanmartin@gmail.com>
2025-04-02 16:38:02 +0100

    RENTNOW.NG

--- src/app/subscribe/page.tsx
+++ src/app/subscribe/page.tsx
@@ -1,23 +1,29 @@
 import Footer from "@/features/landing/components/Footer";
 import Header from "@/features/landing/components/Header";
 import Subscribe from "@/features/landing/components/sections/Subscribe";
-import { baseURL } from "@/../next.config";
-import { PlansInterface } from "@/types/subscription";
+import {baseURL} from "@/../next.config";
+import {PlansInterface} from "@/types/subscription";
+import React, {Suspense} from "react";
+import {Loader} from "lucide-react";
 
 
-const Page = async()=>{ 
- const response = await fetch(baseURL+'/plans');
- const json = await response.json();
- const data:PlansInterface[] = json.data; 
+const Page = async () => {
+    const response = await fetch(baseURL + '/plans');
+    const json = await response.json();
+    const data: PlansInterface[] = json.data;
 
-  return <>
-    <div>
-      <Header/>
-      {data &&<Subscribe plans={data} />}
-      <Footer/>
-    </div>
-   
-</>
+    return <>
+        <div>
+            <Header/>
+            <Suspense fallback={<div className="flex justify-center items-center min-h-screen"><Loader
+                className="animate-spin"/>&nbsp;Loading...</div>}>
+
+                {data && <Subscribe plans={data}/>}
+            </Suspense>
+            <Footer/>
+        </div>
+
+    </>
 
 }
 
