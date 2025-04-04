91e78163cc645039efcbd1da58acb22c5850aa37
=Doubleprincez <=adenekanmartin@gmail.com>
2025-04-04 11:59:39 +0100

    Adding SEO and Limitation to Client Like

--- src/app/find-homes/[id]/page.tsx
+++ src/app/find-homes/[id]/page.tsx
@@ -2,15 +2,14 @@ import {Suspense} from 'react';
 import ApartmentClient from '@/features/landing/components/ApartmentClient';
 import {Loader2Icon} from 'lucide-react';
 import {AxiosApiServer} from "@/lib/server-utils";
-import {ApiSubscriptionResponse} from "@/types/subscription";
 import {baseURL} from "@/../next.config";
 import {Metadata} from "next";
 
-export default async function Page({params}:any) {
+export default async function Page({params}: any) {
 
-    const { id } = await params;
-    const response = await AxiosApiServer().get<ApiSubscriptionResponse>(`${baseURL}/apartment/${id}`);
-    const apartment = response.data;
+    const {id} = await params;
+    const response = await AxiosApiServer().get(`${baseURL}/apartment/${id}`);
+    const apartment = response.data.data;
 
     // Dynamically update metadata based on fetched apartment data
     const metadata: Metadata = {
@@ -46,14 +45,14 @@ export default async function Page({params}:any) {
             {/* Example: */}
             <head>
                 <title>{metadata.title}</title>
-                <meta name="description" content={metadata.description} />
+                <meta name="description" content={metadata.description}/>
                 {/* Add OpenGraph and Twitter meta tags here */}
             </head>
 
             <Suspense fallback={<div className="flex justify-center items-center min-h-screen">
-                <Loader2Icon className="animate-spin" /> &nbsp;Loading...
+                <Loader2Icon className="animate-spin"/> &nbsp;Loading...
             </div>}>
-                <ApartmentClient prevApartment={apartment} />
+                <ApartmentClient prevApartment={apartment}/>
             </Suspense>
         </>
     );
