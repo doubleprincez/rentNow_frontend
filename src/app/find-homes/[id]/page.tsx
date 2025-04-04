193b459c61a744c58812153bc57fb5304019504b
Doubleprincez <adenekanmartin@gmail.com>
2025-04-04 11:26:33 +0100

    Adding SEO and Limitation to Client Like

--- src/app/find-homes/[id]/page.tsx
+++ src/app/find-homes/[id]/page.tsx
@@ -5,24 +5,16 @@ import {AxiosApiServer} from "@/lib/server-utils";
 import {ApiSubscriptionResponse} from "@/types/subscription";
 import {baseURL} from "@/../next.config";
 
-const findApartment = async (apartmentId: string) => {
-    try {
-        const response = await AxiosApiServer().get<ApiSubscriptionResponse>(
-            `${baseURL}/apartment/${apartmentId}`
-        );
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-};
-
 export default async function Page({ params }: { params: { id: string } }) {
-    const apartment = await findApartment(params.id);
+
+    const response = await AxiosApiServer().get<ApiSubscriptionResponse>(
+        `${baseURL}/apartment/${params.id}`
+    );
 
     return (
         <Suspense fallback={<div className="flex justify-center items-center min-h-screen">
             <Loader2Icon className="animate-spin" /> &nbsp;Loading...</div>}>
-            <ApartmentClient prevApartment={apartment} />
+            <ApartmentClient prevApartment={response.data} />
         </Suspense>
     );
 }
