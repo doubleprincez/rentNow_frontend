918e8a67a7d39f36e22356515871af64f2d700a2
gstplace <globalstoresict@gmail.com>
2025-09-13 19:20:26 +0100

    Adding Recently Uploaded to Admin Page

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type {NextConfig} from "next";
 
 // LOCAL
-export const frontendURL = 'http://localhost:3000';
-export const backendUrl = 'http://localhost:8000';
-export const PAYSTACK_PUBLIC_KEY = "pk_test_d196b71372349b838d7a0aa834a9d359874f1ac3"
+// export const frontendURL = 'http://localhost:3000';
+// export const backendUrl = 'http://localhost:8000';
+// export const PAYSTACK_PUBLIC_KEY = "pk_test_d196b71372349b838d7a0aa834a9d359874f1ac3"
 
-// export const   frontendURL = 'https://rentnow.ng';
-// export const     backendUrl = 'https://app.rentnow.ng';
-// export const     PAYSTACK_PUBLIC_KEY = "pk_live_eeefde6e6c7a8c005b5d1ec9eeef98550543d535"
+export const   frontendURL = 'https://rentnow.ng';
+export const     backendUrl = 'https://app.rentnow.ng';
+export const     PAYSTACK_PUBLIC_KEY = "pk_live_eeefde6e6c7a8c005b5d1ec9eeef98550543d535"
 
 export const MAILCHIMP_API_KEY = "9f9e0b3216195985c0d179ad071dc65a"
 export const MAILCHIMP_AUDIENCE_ID = "c7f48b07e2"
