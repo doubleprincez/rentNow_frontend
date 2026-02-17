5441e6f6073b32198baae710c3f461f2400a52d2
doubleprincez <adenekanmartin@gmail.com>
2026-02-17 20:01:00 +0800

    update select viewww

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -9,7 +9,7 @@ const alertVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-background text-foreground",
+        default: "bg-gray-200 text-foreground",
         destructive:
           "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
       },
